c36d868bf37d536ad998735dfd3f4b679b1942cf
AlekLedesma <55155084+AlekLedesma@users.noreply.github.com>
2019-09-23 10:49:46 -0700

    9.23.19

--- BlackJack/Game.java
+++ BlackJack/Game.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Player
+public class Game
 {
     public static void main ()
     {
--- BlackJack/Hand.java
+++ BlackJack/Hand.java
@@ -10,29 +10,30 @@ public class Hand
 {
     private int card1;
     private int card2;
-    public Hand()
+    private int total;
+
+        public Hand()
     {
-        double num1 = Math.random()*10 + 1;
-        card1 = (int) num1;
-        double num2 = Math.random()*10 + 1;
-        card2 = (int) num2;
+        
+        card1 = hit();
+     
+        card2 = hit();
+        total = card1 + card2;
     }
     
-    public void hit()
+    public int hit()
     {
-        int card3;
-        double num3 = Math.random()*10 + 1;
-        card3 = (int) num3;
-        
-        int hit = (int) card1 + card2 + card3;
-        if (hit < 21)
-        {
-            System.out.println("bust");
-        }
+
+        double newNum = Math.random()*10 + 1;
+        int card = (int) newNum;
+        total += card;
+        return card;
+
     }
-    public void stay()
-    {
-        int stay = (int) card1 + card2;
-    }
+    
+
+    
+    
+
     //if (hand > 21){System.out.println("you went over the limit")}
 }
