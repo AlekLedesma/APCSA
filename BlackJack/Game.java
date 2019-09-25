e9412cc0bd4c4ed9c6d68fe7114d1fe7575f9367
AlekLedesma <55155084+AlekLedesma@users.noreply.github.com>
2019-09-25 10:49:51 -0700

    9.25.19

--- BlackJack/Game.java
+++ BlackJack/Game.java
@@ -9,13 +9,45 @@ import java.util.Scanner;
 public class Game
 
 {
+    
     public static void main ()
     {
         Hand dealerHand = new Hand();
         Hand playerHand = new Hand();
         Scanner scanner = new Scanner (System.in);
-        System.out.println(playerHand.Card1());
-        //while loop here
+        System.out.println("player drew " + playerHand.Card1() + " & "  + playerHand.Card2());
+        boolean stay = false;
+        //boolean bust = false;
+        //boolean compare = false;
+
+        while (playerHand.Total() < 21 && !stay)
+        {
+            String hitScanner = scanner.nextLine();
+            if(hitScanner.equals("hit"))
+            {
+                playerHand.hit();
+                System.out.println("player got a " + playerHand.hit());
+                System.out.println("new player total: " + playerHand.Total());
+            }else
+            {
+                stay = true;
+                System.out.println("player stays");
+            }
+        }
+        if (playerHand.Total() > 21)
+        {
+            System.out.println("Player busted");
+        }
+        while (dealerHand.Total() <= 17 && !stay )
+        {
+            dealerHand.hit();
+            System.out.println("dealer total: " + dealerHand.Total());
+        }
+        if (dealerHand.Total() >= 17)
+        {
+            stay = true;
+        }
+        }
     }
 
-}
+
