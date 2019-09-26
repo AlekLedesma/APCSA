51ebb6e8e65c732bf20b48b9283a697e34636c22
635r <635r@wetigers.org>
2019-09-26 03:56:58 -0700

    9.26.19
    working on guess game

--- AlekLedesma - GuessGame/GuessGame.java
+++ AlekLedesma - GuessGame/GuessGame.java
@@ -13,22 +13,24 @@ public class GuessGame
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        
         double num = Math.random()*10 + 1;
         int newNum = (int) num;
-        
-        for (int i = 5);
-        
-        
-          
-        
-            
-        
-        
-        
-        
-        
-
+        for (int chances = 5; chances > 5; chances--)
+        {
+            int inputScanner = scanner.nextInt();
+            System.out.println("you have " + chances + "chances left");
+            if (inputScanner == newNum)
+            {
+                System.out.println("You got guessed the right number");
+            }else if (inputScanner > newNum)
+            {
+                 System.out.println("too high");
+            }
+            else if (inputScanner < newNum)
+            {
+                System.out.println("too low");
+            }
+        }
     }
 }    
 
