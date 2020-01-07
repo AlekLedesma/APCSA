0007ce150418bf6ae9e731e2a5cac6f15e8b0d7c
AlekLedesma <55155084+AlekLedesma@users.noreply.github.com>
2020-01-07 10:50:33 -0800

    1.7.19
    first of the year

---
+++ Notes/TwoDArrays.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class TwoDArrays here.
+ *
+ * @author (Alek Ledesma)
+ * @version (1.7.19)
+ */
+public class TwoDArrays
+{
+    int[] array = new int[10];
+    int[][] tgArray = new int[10][10];
+    int[][][][][][][][][][] tenD;
+    //    r  c 
+    //int[a][b] Row-Major-Format
+    //starts at row 0, column 0
+    //  r = y axis   c = x axis
+
+    public TwoDArrays()
+    {
+        tdArray[0][0] = 1;
+        tdArray[1][3] = 4;
+    }
+        public void numberAllCells(){
+            int num = 1;
+            for(int i = 0; i < tdArray.length; i++)
+            {
+                
+            }
+        }
+}
+
