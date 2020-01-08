49fcd3321aeaa0d2a13e49b4c359468ab724214d
AlekLedesma <55155084+AlekLedesma@users.noreply.github.com>
2020-01-08 10:49:44 -0800

    1.8.20

--- Notes/TwoDArrays.java
+++ Notes/TwoDArrays.java
@@ -8,10 +8,11 @@
 public class TwoDArrays
 {
     int[] array = new int[10];
-    int[][] tgArray = new int[10][10];
+    int[][] tdArray = new int[10][10];
+    int[][] otherArr = new int[5][10];
     int[][][][][][][][][][] tenD;
     //    r  c 
-    //int[a][b] Row-Major-Format
+    //int[a][b] ROW-MAJOR-FORMAT
     //starts at row 0, column 0
     //  r = y axis   c = x axis
 
@@ -20,12 +21,42 @@ public class TwoDArrays
         tdArray[0][0] = 1;
         tdArray[1][3] = 4;
     }
-        public void numberAllCells(){
-            int num = 1;
-            for(int i = 0; i < tdArray.length; i++)
+    public void numberAllCells(){
+        int num = 1;
+        for(int r = 0; r < tdArray.length; r++)
+        {
+            for(int c = 0; c < tdArray[0].length; c++)
             {
-                
+                tdArray[r][c] = num;
+                num++;
             }
         }
+        }
+    public int addAllNumbers()
+    {
+        int total = 0;
+        for(int r = 0; r < tdArray.length; r++)
+        {
+            for(int c = 0; c < tdArray[0].length; c++)
+            {
+               total += tdArray[r][c]; 
+            }
+        }
+        return total;
+    }
+    public int addAllNumbers2()
+    {
+        //this has an Enhanced loop
+        //Enhanced loops can't be used to chnage values in an array
+        int total = 0;
+        for(int[] oneArr : tdArray)
+        {
+            for(int num : oneArr)
+            {
+               total += num; 
+            }
+        }
+        return total;
+    }
 }
 
