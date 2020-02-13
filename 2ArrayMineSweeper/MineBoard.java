50f3a0c9cc4fc4072d1f934ff0ca709dd1863ea7
AlekLedesma <55155084+AlekLedesma@users.noreply.github.com>
2020-02-13 10:49:39 -0800

    2.13.20

--- 2ArrayMineSweeper/MineBoard.java
+++ 2ArrayMineSweeper/MineBoard.java
@@ -51,108 +51,115 @@ public class MineBoard
     public void addNums()
     {
         int number = 0;
-        for(int i = 0; i < rows * columns; i++)
+        for(int i = 0; i < board.length; i++)
         {
-            if(!board[i].isBomb())
+            for(int j = 0; j < board[0].length; j++)
             {
-                //Top
-                if(i - columns >= 0 )
+                if(!board[i][j].isBomb())
                 {
-                    if(board[i - columns].isBomb())
+                    //Top
+                    if(i - columns >= 0 )
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i - columns][j - rows].isBomb())
+                        {
+                            board[i][j].value = board[i][j - rows].value + 1;
+                        }
                     }
-                }
-                //Bottom
-                if(i + columns < board.length )
-                {
-                    if(board[i + columns].isBomb())
+                    //Bottom
+                    if(i + columns < board.length )
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i + columns][j + rows].isBomb())
+                        {
+                            board[i][j].value = board[i][j].value + 1;
+                        }
                     }
-                }
-                //Left
-                if (i - 1 >= 0 && i % columns != 0 )
-                {
-                    if(board[i - 1].isBomb())
+                    //Left
+                    if (i - 1 >= 0 && i % columns != 0 )
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i - 1][j - 1].isBomb())
+                        {
+                            board[i][j].value = board[i][j].value + 1;
+                        }
                     }
-                }
-                //Right
-                if (i + 1 < board.length && i % columns != columns - 1 )
-                {
-                    if(board[i + 1].isBomb())
+                    //Right
+                    if (i + 1 < board.length && i % columns != columns - 1 )
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i + 1][j + 1].isBomb())
+                        {
+                            board[i][j].value = board[i][j].value + 1;
+                        }
                     }
-                }
-                //TopRight
-                if (i - columns + 1 >= 0 && i % columns != columns - 1)
-                {
-                    if(board[i - columns + 1].isBomb())
+                    //TopRight
+                    if (i - columns + 1 >= 0 && i % columns != columns - 1)
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i - columns + 1][j - rows + 1].isBomb())
+                        {
+                            board[i][j].value = board[i][j].value + 1;
+                        }
                     }
-                }
-                //TopLeft
-                if (i - columns - 1 >= 0 && i % columns != 0)
-                {
-                    if(board[i - columns - 1].isBomb())
+                    //TopLeft
+                    if (i - columns - 1 >= 0 && i % columns != 0)
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i - columns - 1][i - rows - 1].isBomb())
+                        {
+                            board[i][j].value = board[i][j].value + 1;
+                        }
                     }
-                }
-                //BottomRight
-                if (i + columns + 1 < board.length && i % columns != columns - 1)
-                {
-                    if(board[i + columns + 1].isBomb())
+                    //BottomRight
+                    if (i + columns + 1 < board.length && i % columns != columns - 1)
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i + columns + 1][i + rows + 1].isBomb())
+                        {
+                            board[i][j].value = board[i][j].value + 1;
+                        }
                     }
-                }
-                //BottomLeft
-                if (i + columns - 1 < board.length && i % columns != 0)
-                {
-                    if(board[i + columns - 1].isBomb())
+                    //BottomLeft
+                    if (i + columns - 1 < board.length && i % columns != 0)
                     {
-                        board[i].value = board[i].value + 1;
+                        if(board[i + columns - 1][i + rows - 1].isBomb())
+                        {
+                            board[i][j].value = board[i][j].value + 1;
+                        }
                     }
                 }
             }
         }
     }
-        /**This method is used for testing and will be deleted if using the GUI.
-         *  It is still required for all students.
-         */
-        public void printBoard()
+    /**This method is used for testing and will be deleted if using the GUI.
+     *  It is still required for all students.
+     */
+    public void printBoard()
+    {
+        
+        for(int i = 0; i < rows; i++)
         {
-            int p = 0;
-            for(int i = 0; i < rows; i++)
+            for(int j = 0; j < columns; j++)
             {
-                for(int o = 0; o < columns; o++)
+                if(board[i][j].isBomb())
                 {
-                    if(board[p].isBomb())
-                    {
-                        System.out.print("X ");
-                    }else
-                    {
-                        System.out.print(board[p].value + " ");
-                    }
-                    p++;
+                    System.out.print("X ");
+                }else
+                {
+                    System.out.print(board[i][j].value + " ");
                 }
-                System.out.println();
-            } 
-        }
-        public JPanel addCells()
-        {
-            JPanel panel = new JPanel(new GridLayout(rows,columns));
-            for(int i = 0; i< rows*columns; i++)
-            {
-                board[i]= new Cell();
-                panel.add(board[i].getButton());
+                i++;
+                j++;
             }
-            return panel;
-        }
-
+            System.out.println();
+        } 
     }
+    public JPanel addCells()
+    {
+        JPanel panel = new JPanel(new GridLayout(rows,columns));
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[0].length; j++)
+            {
+                board[i][j]= new Cell();
+                panel.add(board[i][j].getButton());
+            }
+        }
+        
+        return panel;
+    }
+}
