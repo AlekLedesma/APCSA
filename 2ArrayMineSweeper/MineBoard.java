c808495b1f13f58ebe76b858b64d248420f4fab6
AlekLedesma <55155084+AlekLedesma@users.noreply.github.com>
2020-02-14 10:50:10 -0800

    2.14.20

--- 2ArrayMineSweeper/MineBoard.java
+++ 2ArrayMineSweeper/MineBoard.java
@@ -60,31 +60,31 @@ public class MineBoard
                     //Top
                     if(i - columns >= 0 )
                     {
-                        if(board[i - columns][j - rows].isBomb())
+                        if(board[i - 1][j].isBomb())
                         {
-                            board[i][j].value = board[i][j - rows].value + 1;
+                            board[i][j].value = board[i][j].value + 1;
                         }
                     }
                     //Bottom
                     if(i + columns < board.length )
                     {
-                        if(board[i + columns][j + rows].isBomb())
+                        if(board[i + 1][j].isBomb())
                         {
                             board[i][j].value = board[i][j].value + 1;
                         }
                     }
                     //Left
-                    if (i - 1 >= 0 && i % columns != 0 )
+                    if (j - 1 >= 0 )
                     {
-                        if(board[i - 1][j - 1].isBomb())
+                        if(board[i][j - 1].isBomb())
                         {
                             board[i][j].value = board[i][j].value + 1;
                         }
                     }
                     //Right
-                    if (i + 1 < board.length && i % columns != columns - 1 )
+                    if (j + 1 < board[0].length )
                     {
-                        if(board[i + 1][j + 1].isBomb())
+                        if(board[i][j + 1].isBomb())
                         {
                             board[i][j].value = board[i][j].value + 1;
                         }
@@ -92,7 +92,7 @@ public class MineBoard
                     //TopRight
                     if (i - columns + 1 >= 0 && i % columns != columns - 1)
                     {
-                        if(board[i - columns + 1][j - rows + 1].isBomb())
+                        if(board[i - 1][j + 1].isBomb())
                         {
                             board[i][j].value = board[i][j].value + 1;
                         }
@@ -100,7 +100,7 @@ public class MineBoard
                     //TopLeft
                     if (i - columns - 1 >= 0 && i % columns != 0)
                     {
-                        if(board[i - columns - 1][i - rows - 1].isBomb())
+                        if(board[i - 1][j - 1].isBomb())
                         {
                             board[i][j].value = board[i][j].value + 1;
                         }
@@ -108,7 +108,7 @@ public class MineBoard
                     //BottomRight
                     if (i + columns + 1 < board.length && i % columns != columns - 1)
                     {
-                        if(board[i + columns + 1][i + rows + 1].isBomb())
+                        if(board[i + 1][j + 1].isBomb())
                         {
                             board[i][j].value = board[i][j].value + 1;
                         }
@@ -116,7 +116,7 @@ public class MineBoard
                     //BottomLeft
                     if (i + columns - 1 < board.length && i % columns != 0)
                     {
-                        if(board[i + columns - 1][i + rows - 1].isBomb())
+                        if(board[i + 1][i - 1].isBomb())
                         {
                             board[i][j].value = board[i][j].value + 1;
                         }
@@ -131,9 +131,9 @@ public class MineBoard
     public void printBoard()
     {
         
-        for(int i = 0; i < rows; i++)
+        for(int i = 0; i < board.length; i++)
         {
-            for(int j = 0; j < columns; j++)
+            for(int j = 0; j < board[0].length; j++)
             {
                 if(board[i][j].isBomb())
                 {
@@ -142,11 +142,26 @@ public class MineBoard
                 {
                     System.out.print(board[i][j].value + " ");
                 }
-                i++;
-                j++;
             }
             System.out.println();
         } 
+        
+        for (Cell[] cells : board)
+        {
+            for (Cell cell : cells)
+            {
+                if (cell.isBomb())
+                {
+                    System.out.print("X ");
+                }
+                else
+                {
+                    System.out.print(cell.value + " ");
+                }
+            }
+            
+            System.out.println();
+        }
     }
     public JPanel addCells()
     {
